e6a676c325eedf4d649e5e8e5dfaec40790287cb
ntdat19492-lang <ntdat19492@gmail.com>
2025-12-14 01:56:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-// JavaScript để thay đổi giữa Đăng Nhập và Đăng Ký
-
-// Lắng nghe sự kiện click vào nút chuyển sang Đăng Ký
 document.getElementById('mopopupDangKy').addEventListener('click', function() {
-  // Ẩn phần Đăng Nhập
   document.getElementById('dangNhap').style.display = 'none';
-
-  // Hiển thị phần Đăng Ký
   document.getElementById('dangKy').style.display = 'flex';
 });
 
-// Lắng nghe sự kiện click vào nút chuyển sang Đăng Nhập
 document.getElementById('mopopupDangNhap').addEventListener('click', function() {
-  // Ẩn phần Đăng Ký
   document.getElementById('dangKy').style.display = 'none';
-
-  // Hiển thị phần Đăng Nhập
   document.getElementById('dangNhap').style.display = 'flex';
+});
+
+document.querySelectorAll('input').forEach(input => {
+  input.addEventListener('focus', function() {
+    this.style.color = '#999999';  // Đổi màu chữ khi focus
+  });
+  input.addEventListener('blur', function() {
+    if (!this.value) {
+      this.style.color = 'white';  // Trả lại màu chữ khi mất focus
+    }
+  });
 });
