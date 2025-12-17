b631e75b5d6db1022bff846c615248e8e365d2f2
ntdat19492-lang <ntdat19492@gmail.com>
2025-12-18 03:02:30 +0700

    Create login.js

---
+++ functions/login.js
@@ -0,0 +1,34 @@
+export async function onRequestPost({ request }) {
+  try {
+    const body = await request.json();
+    const gasUrl = "https://script.google.com/macros/s/AKfycbwi-porgZXeTWAZ7MoAUXYzqJAL9Eh7wbcUV2ItAnWHLfYeTIQLeLiTkn9RmFEUVhiuMQ/exec";
+
+    const gasRes = await fetch(gasUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        action: "login",
+        username: body.username,
+        password: body.password
+      })
+    });
+
+    let gasJson = null;
+
+    try {
+      gasJson = await gasRes.json();
+    } catch (err) {
+      gasJson = { ok: false, error: "GAS không trả JSON", raw: await gasRes.text() };
+    }
+
+    return new Response(JSON.stringify({gasJson}), {headers: { "Content-Type": "application/json" }});
+
+  } catch (err) {
+    return new Response(JSON.stringify({
+      ok: false,
+      error: "Lỗi Cloudflare: " + err.message
+    }), {
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
